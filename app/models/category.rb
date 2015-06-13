42d34a8eff19307494cc72a14b8af4edc5b715bc
Karol <karol.lipski@gmail.com>
2015-06-13 20:57:27 +0000

    category tree

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,10 @@
 class Category < ActiveRecord::Base
   has_many :subcategories, :class_name => 'Category', :foreign_key => "parent_id"
   belongs_to :parent , :class_name => 'Category'
+  
+  validates_presence_of :name
+
+  def self.main
+    all.where(parent: nil)
+  end
 end
