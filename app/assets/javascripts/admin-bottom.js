576a5c13550fb261295aaac89fde01f404c0cdfb
Karol <karol.lipski@gmail.com>
2015-06-20 17:37:12 +0000

    layout

---
+++ app/assets/javascripts/admin-bottom.js
@@ -0,0 +1,29 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require neon/js/gsap/main-gsap
+//= require neon/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min
+//= require neon/js/bootstrap
+//= require neon/js/joinable
+//= require neon/js/resizeable
+//= require neon/js/neon-api
+//= require neon/js/jvectormap/jquery-jvectormap-1.2.2.min
+//= require neon/js/jvectormap/jquery-jvectormap-europe-merc-en
+//= require neon/js/jquery.sparkline.min
+//= require neon/js/rickshaw/vendor/d3.v3
+//= require neon/js/rickshaw/rickshaw.min
+//= require neon/js/raphael-min
+//= require neon/js/morris.min
+//= require neon/js/toastr
+//= require neon/js/neon-chat
+//= require neon/js/neon-custom
+//= require neon/js/neon-demo
