2a77eadeaa47a8290340b7a080c38c54d1eb0a3c
Karol Lipski <karol.lipski@gmail.com>
2015-06-25 12:57:45 +0200

    Add postgresql support

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150528223134) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.integer  "parent_id"
