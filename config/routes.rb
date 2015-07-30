e823a867d3a74cf32222a44d11da5a1b2df32980
Karol Lipski <karol.lipski@gmail.com>
2015-07-30 22:54:37 +0200

    items controller and nested route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root to: 'main#index'
 
-  resources :categories
+  resources :categories do
+    resources :items, only: [:index]
+  end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
