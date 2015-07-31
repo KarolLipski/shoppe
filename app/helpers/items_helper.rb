b5d01e32efa2281acab8f589a33206fc9b81ce5b
Karol Lipski <karol.lipski@gmail.com>
2015-08-01 00:21:57 +0200

    render items

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,7 @@
 module ItemsHelper
+
+  def render_price(item)
+    number_to_currency item.price , precision: 2
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,8 @@ class Item < ActiveRecord::Base
 
   validates_presence_of :name, :number, :small_wrap, :big_wrap
   validates_numericality_of :small_wrap, :big_wrap, :only_integer => true, :greater_than_or_equal_to => 0
+
+  def price
+    99.21
+  end
 end
