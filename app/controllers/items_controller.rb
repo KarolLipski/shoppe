d7c8302d32f1bc0f323fb6fdbad835428164eb4e
Karol Lipski <karol.lipski@gmail.com>
2015-08-02 12:52:43 +0200

    paginator template

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   # GET /categories/:category_id/items
   def index
     @category = Category.find(params[:category_id])
-    @items = @category.items.page(params[:page]).per(20)
+    @items = @category.items.page(params[:page]).per(25)
   end
 
 end
