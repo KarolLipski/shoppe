b8338da7edba76e19aa324fa67b2e39dd5124df7
Karol Lipski <karol.lipski@gmail.com>
2015-08-02 12:20:18 +0200

    add paginator for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,8 @@ class ItemsController < ApplicationController
 
   # GET /categories/:category_id/items
   def index
-    category = Category.find(params[:category_id])
-    @items = category.items
+    @category = Category.find(params[:category_id])
+    @items = @category.items.page(params[:page]).per(20)
   end
 
 end
