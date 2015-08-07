7747c588f1b499a4bb5deaad6b2874d59f8b3c2d
Karol Lipski <karol.lipski@gmail.com>
2015-08-07 23:55:07 +0200

    items controlller index spec

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
 
+  describe 'GET #index' do
+    it "assigns category" do
+      category = FactoryGirl.create(:category)
+      get :index, category_id: category.id
+      expect(assigns(:category)).to eq(category)
+    end
+  end
+
 end
