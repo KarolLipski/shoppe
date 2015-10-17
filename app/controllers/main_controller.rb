43b5577965f6222f7ae237c7325c10ded90b7feb
Karol Lipski <karol.lipski@gmail.com>
2015-10-17 19:52:40 +0200

    slider  and front page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,9 @@
 class MainController < ApplicationController
 
   def index
+    @last_added = Item.active.with_photo.order(created_at: :desc).take(6)
 
+    @recommend = Item.active.with_photo.order("RAND()").take(9)
   end
 
 end
