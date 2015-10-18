09aa878e3fe4432250bd372bf06ed887addbe190
Karol Lipski <karol.lipski@gmail.com>
2015-10-18 23:10:34 +0200

    more last added items

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,8 @@
 class MainController < ApplicationController
 
   def index
-    @last_added = Item.active.with_photo.order(created_at: :desc).take(6)
+    @last_added = Item.active.with_photo.order(created_at: :desc)
+                      .take(30).shuffle.first(6)
 
     @recommend = Item.active.with_photo.order("RAND()").take(9)
   end
