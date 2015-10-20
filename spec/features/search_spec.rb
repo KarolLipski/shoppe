0123b96d31a800cc6546287b5719f865443580e8
Karol Lipski <karol.lipski@gmail.com>
2015-10-20 23:20:08 +0200

    search feature spec

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -5,24 +5,28 @@ feature 'Search' do
   before(:each) do
     prepre_items
     visit root_path
-    fill_in 'search', with: 'test'
-    click_button 'search'
   end
 
   scenario 'request should be GET' do
+    fill_in 'search', with: 'test'
+    click_button 'search'
     uri = URI.parse(current_url)
     expect("#{uri.path}?#{uri.query}").to match(/search=test/)
   end
 
   feature 'when items exists' do
       scenario 'show page with results' do
-
+        fill_in 'search', with: 'test'
+        click_button 'search'
+        expect(page).to have_css('div.single-products', count: 3)
       end
   end
 
   feature 'when items not exists' do
     scenario 'show no result information' do
-
+      fill_in 'search', with: 'no item'
+      click_button 'search'
+      expect(page).to have_content('Brak wyników')
     end
   end
 end
