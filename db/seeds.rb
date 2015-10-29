82e7d91d7445d1a0bd5eb76e764a74cdf18c4590
Karol Lipski <karol.lipski@gmail.com>
2015-10-29 23:23:22 +0100

    admin user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,5 @@
 #Dir[File.join(Rails.root,'db','seeds','*.rb')].sort.each { |seed| load seed }
 
 # load File.join(Rails.root,'db','seeds','01_categories.rb')
-load File.join(Rails.root,'db','seeds','02_items.rb')
+# load File.join(Rails.root,'db','seeds','02_items.rb')
+# load File.join(Rails.root,'db','seeds','03_users.rb')
---
+++ db/seeds/03_users.rb
@@ -0,0 +1,8 @@
+#Admin
+User.create(
+        name: 'admin',
+        email: 'admin@gmail.com',
+        login: 'admin',
+        password: 'xxx',
+        password_confirmation: 'xxx'
+)
