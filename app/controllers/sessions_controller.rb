9296be323d0ae179eb2ed0a68fa7290b6d95f9ff
Karol Lipski <karol.lipski@gmail.com>
2015-10-31 04:33:36 +0100

    session create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,15 @@ class SessionsController < ApplicationController
   def new
 
   end
+
+  def create
+    user = User.find_by(name: params[:session][:login])
+    if user && user.authenticate(params[:session][:password])
+      redirect_to root_path
+    else
+      flash[:danger] = "Login lub hasło jest nie poprawne"
+      render :new
+    end
+  end
+
 end
