65e61aacaba5af8c0b8fe07bd8c212d21dab26da
Karol Lipski <karol.lipski@gmail.com>
2015-11-17 21:05:46 +0100

    edit user feature spec

--- spec/features/admin/user_manage_spec.rb
+++ spec/features/admin/user_manage_spec.rb
@@ -14,3 +14,36 @@ feature 'users listing' do
     end
   end
 end
+
+feature 'edit users' do
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    visit edit_admin_user_path @user
+  end
+  context 'when data is valid' do
+    before(:each) do
+      fill_in 'name', with: 'firma'
+      fill_in 'contractor_sym', with: '203'
+      fill_in 'receiver_sym', with: '203'
+      fill_in 'login', with: 'login'
+      fill_in 'email', with: 'email@wp.pl'
+      click_button 'Zapisz'
+    end
+    scenario 'display success info' do
+      within('.alert-success') do
+        expect(page).to have_content('Zapisano poprawnie')
+      end
+    end
+    scenario 'redirect to users list' do
+        expect(page).to have_selector 'h3', text: 'Klienci'
+    end
+  end
+  context 'when data is invalid' do
+    scenario 'display error info' do
+      click_button 'Zapisz'
+      within('alert-danger') do
+        expect(page). to have_content('Błąd podczas zapisu')
+      end
+    end
+  end
+end
