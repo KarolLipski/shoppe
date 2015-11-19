8242d6cbaa84eba21c4d9d8ec21ae2fddf24cca3
Karol Lipski <karol.lipski@gmail.com>
2015-11-19 23:47:22 +0100

    create user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,6 +10,23 @@ class Admin::UsersController < AdminController
     @user = User.find(params[:id])
   end
 
+  #GET admin/users/new
+  def new
+    @user = User.new
+  end
+
+  #POST admin/users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:succes] = 'KLient został dodany'
+      redirect_to admin_users_path
+    else
+      flash[:danger] = 'Błąd zapisu'
+      render :new
+    end
+  end
+
   #PUT admin/users/:id
   def update
     @user = User.find(params[:id])
