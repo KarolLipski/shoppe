266b9054c6e8df1f85a876c7a5f79a30e9b5ca1a
Karol Lipski <karol.lipski@gmail.com>
2015-12-03 00:02:32 +0100

    remove console logs

--- app/assets/javascripts/carts.js
+++ app/assets/javascripts/carts.js
@@ -3,8 +3,6 @@ jQuery(document).ready(function($)
     $('.cart_quantity_input').on('change', function() {
         var new_quantity = this.value;
         var cart_item_id = $(this).attr("cart_item_id");
-        console.log(new_quantity);
-        console.log($(this).attr("cart_item_id"));
 
         $.ajax({
             url: '/cart_items/'+cart_item_id+'.json',
@@ -12,14 +10,11 @@ jQuery(document).ready(function($)
             data: "id="+cart_item_id+"&quantity="+new_quantity+"",
             success: function(data) {
                 $("#cart-item-error-"+cart_item_id).text('');
-                console.log(data);
                 if(data.errors) {
                     toastr.error('Wystąpił bład','', {"closeButton": true});
-                    console.log(data.errors[0]);
                     $("#cart-item-error-"+cart_item_id).text(data.errors[0]);
                 }
                 else {
-                    console.log(data.sum);
                     $("#cart-sum").text(data.sum);
                     toastr.success('Ilość została zmieniona. <br />Wartośc koszyka: '
                         +data.sum+' zł.','', {"closeButton": true});
