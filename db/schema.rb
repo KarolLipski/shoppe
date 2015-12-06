9253157c102c368642f1b88ac597a5af23eeda56
Karol Lipski <karol.lipski@gmail.com>
2015-12-06 14:32:13 +0100

    order base structures

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201214841) do
+ActiveRecord::Schema.define(version: 20151206132558) do
 
   create_table "actualization_logs", force: :cascade do |t|
     t.string   "status",      limit: 255
@@ -84,6 +84,26 @@ ActiveRecord::Schema.define(version: 20151201214841) do
     t.datetime "updated_at",             null: false
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.integer  "order_id",    limit: 4
+    t.integer  "quantity",    limit: 4
+    t.decimal  "price",                 precision: 10, scale: 2
+    t.decimal  "total_price",           precision: 10, scale: 2
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+  end
+
+  add_index "order_items", ["order_id"], name: "index_order_items_on_order_id", using: :btree
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.decimal  "price",                precision: 10, scale: 2
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
+  end
+
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
+
   create_table "stored_items", force: :cascade do |t|
     t.integer  "magazine_id", limit: 4
     t.integer  "item_id",     limit: 4
@@ -113,6 +133,8 @@ ActiveRecord::Schema.define(version: 20151201214841) do
   add_foreign_key "cart_items", "items"
   add_foreign_key "carts", "users"
   add_foreign_key "items", "categories"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "users"
   add_foreign_key "stored_items", "items"
   add_foreign_key "stored_items", "magazines"
 end
