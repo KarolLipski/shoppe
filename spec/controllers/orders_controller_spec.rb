89c6c971c7b578437c60c6bb003cb7c75d0ae722
Karol Lipski <karol.lipski@gmail.com>
2015-12-17 14:42:52 +0100

    order controller specs 1

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -2,56 +2,113 @@ require 'rails_helper'
 
 RSpec.describe OrdersController, type: :controller do
 
-  # describe 'POST create' do
-  #   before(:each) do
-  #     @user = FactoryGirl.create(:user)
-  #     log_in @user
-  #     @cart = current_cart
-  #     @cart_items = FactoryGirl.create_list(:cart_item, 3, cart: @cart)
-  #     @quantities = @cart_items.map { |i| [i.id.to_s, "1"]}.to_h
-  #     @cart.reload
-  #     @result = {success: true, order: nil, info: {type: :success, message: 'xxx'}}
-  #   end
-  #   it 'assings current user' do
-  #     post :create, quantities: @quantities
-  #     expect(assigns(:user)).to eq @user
-  #   end
-  #   it 'assigns current cart' do
-  #     post :create, quantities: @quantities
-  #     expect(assigns(:cart)).to eq @cart
-  #   end
-  #   it 'call order service' do
-  #     allow_any_instance_of(CreateOrderService).to receive(:call).and_return(@result)
-  #     post :create, quantities: @quantities
-  #   end
-  #   it 'assigns proper flash' do
-  #     post :create, quantities: @quantities
-  #     expect(flash[@result[:info][:type]]).to be_present
-  #   end
-  #   context 'when all items are valid' do
-  #     before(:each) do
-  #       allow_any_instance_of(CreateOrderService).to receive(:call).and_return(@result)
-  #       post :create, quantities: @quantities
-  #     end
-  #     it 'redirect to home page' do
-  #       expect(response).to redirect_to(cart_show_path)
-  #     end
-  #   end
-  #   context 'when some items are invalid' do
-  #     before(:each) do
-  #       order = FactoryGirl.build(:order)
-  #       order.order_items << FactoryGirl.build(:order_item)
-  #       @result[:success] = false
-  #       @result[:order] = order
-  #       allow_any_instance_of(CreateOrderService).to receive(:call).and_return(@result)
-  #       post :create, quantities: @quantities
-  #     end
-  #     it 'assigns items' do
-  #       expect(assigns(:items)).to eq(@cart.cart_items)
-  #     end
-  #     it 'render cart_show template' do
-  #       expect(response).to render_template 'carts/show'
-  #     end
-  #   end
-  # end
+  describe 'GET new' do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      log_in(@user)
+      @cart = current_cart
+      @cart.cart_items << FactoryGirl.create(:cart_item, cart: @cart)
+      @cart.reload
+    end
+    it 'creates build new Order' do
+      get :new
+      expect(assigns(:order)).to be_a_new(Order)
+    end
+    it 'builds order items based on cart_items' do
+      get :new
+      expect(assigns(:order).order_items.size).to eq(@cart.cart_items.size)
+    end
+    it 'assigns current user' do
+      get :new
+      expect(assigns(:user)).to eq(@user)
+    end
+    it 'assigns current cart' do
+      get :new
+      expect(assigns(:cart)).to eq(@cart)
+    end
+    it 'renders new template' do
+      get :new
+      expect(response).to render_template('new')
+    end
+    it 'calls check_cart' do
+      expect(controller).to receive(:check_cart)
+      get :new
+    end
+  end
+
+  describe 'Check cart before action' do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      log_in(@user)
+      @cart = current_cart
+      @cart.cart_items << FactoryGirl.create(:cart_item, cart: @cart)
+      @cart.reload
+    end
+    context 'when there is no cart' do
+      it 'assings flash' do
+        log_out
+        get :new
+        expect(flash[:warning]).to be_present
+      end
+    end
+    context 'when there is no user ' do
+      it 'assings flash' do
+        log_out
+        get :new
+        expect(flash[:warning]).to be_present
+      end
+    end
+    context 'when cart is empty ' do
+      it 'assings flash' do
+        @cart.cart_items.clear
+        get :new
+        expect(flash[:warning]).to be_present
+      end
+    end
+  end
+
+  describe 'POST create' do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      log_in @user
+      @cart = current_cart
+      @cart_items = FactoryGirl.create_list(:cart_item, 3, cart: @cart)
+      @quantities = @cart_items.map { |i| [i.id.to_s, "1"]}.to_h
+      @cart.reload
+    end
+    it 'assings current user' do
+
+    end
+    it 'assigns current cart' do
+
+    end
+    it 'sets total order price' do
+
+    end
+    context 'when all items are valid' do
+      before(:each) do
+
+      end
+      it 'redirect to home page' do
+
+      end
+      it 'sets success flash' do
+
+      end
+    end
+    context 'when some items are invalid' do
+      before(:each) do
+
+      end
+      it 'sets error flash' do
+
+      end
+      it 'render new template' do
+
+      end
+      it 'doesnt create order' do
+
+      end
+    end
+  end
 end
