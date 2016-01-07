37a677df98aa3f4fd1340d3627fb186a03289d80
Karol Lipski <karol.lipski@gmail.com>
2016-01-08 00:01:05 +0100

    specs refactor

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,4 @@
 class ItemsController < ApplicationController
 
-  # GET /categories/:category_id/items
-  def index
-    @category = Category.find(params[:category_id])
-    @items = @category.items.active.with_photo.order(created_at: :desc).page(params[:page]).per(40)
-  end
-
 
 end
--- app/controllers/stored_items_controller.rb
+++ app/controllers/stored_items_controller.rb
@@ -3,6 +3,6 @@ class StoredItemsController < ApplicationController
   # GET /categories/:category_id/items
   def index
     @category = Category.find(params[:category_id])
-    @items = @category.stored_items.active.page(params[:page]).per(10)
+    @items = @category.stored_items.active.page(params[:page]).per(40)
   end
 end
