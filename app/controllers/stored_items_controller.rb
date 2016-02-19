7be0fd97bb382f59cd52465cc44d42df0362e3a6
Karol Lipski <karol.lipski@gmail.com>
2016-02-19 23:01:15 +0100

    items sort

--- app/controllers/stored_items_controller.rb
+++ app/controllers/stored_items_controller.rb
@@ -3,6 +3,21 @@ class StoredItemsController < ApplicationController
   # GET /categories/:category_id/items
   def index
     @category = Category.find(params[:category_id])
-    @items = @category.stored_items.active.page(params[:page]).per(40)
+    @items = @category.stored_items.active.
+        order(get_sort_type).
+        page(params[:page]).per(40)
+  end
+
+  private
+  # Gets customer ordering
+  def get_sort_type
+    sort, sort_type = 'created_at','DESC'
+    if params[:sort]
+      sort = params[:sort] ? params[:sort] : sort
+      sort_type = params[:sort_type] ? params[:sort_type] : sort_type
+    elsif session[:customer_sort_type]
+      return @sort_type = session[:customer_sort_type]
+    end
+    return session[:customer_sort_type] = @sort_type = "#{sort} #{sort_type}"
   end
 end
--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -4,4 +4,24 @@ module ItemsHelper
     number_to_currency item.send(field) , precision: 2
   end
 
+  #translates order type
+  def sort_label(sort_type)
+    case sort_type
+      when 'created_at DESC'
+        return 'Data: Najnowsze'
+      when 'created_at ASC'
+        return 'Data: Najstarsze'
+      when 'price DESC'
+        return 'Cena: Największa'
+      when 'price ASC'
+        return 'Cena: Najmniejsza'
+      when 'quantity DESC'
+        return 'Ilość: Największa'
+      when 'quantity ASC'
+        return 'Ilość: Najmniejsza'
+      else
+        return '--- Wybierz ---'
+    end
+  end
+
 end
