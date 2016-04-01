9eae522044995f3f9eb47ff0ad32004c6a09a05b
Karol Lipski <karol.lipski@gmail.com>
2016-04-01 20:22:31 +0200

    offer controller spec

--- spec/controllers/admin/offers_controller_spec.rb
+++ spec/controllers/admin/offers_controller_spec.rb
@@ -23,4 +23,99 @@ RSpec.describe Admin::OffersController, type: :controller do
     end
   end
 
+  describe 'GET new' do
+    it 'assigns offer' do
+      get :new
+      expect(assigns(:offer)).to be_a_new(Offer)
+    end
+  end
+
+  describe 'POST create' do
+    let(:attributes) {
+      attributes = FactoryGirl.attributes_for(:offer)
+    }
+    context 'with valid params' do
+      before(:each) do
+        post :create , offer: attributes
+      end
+      it 'save offer' do
+        expect(assigns(:offer).new_record?).to eq false
+      end
+      it 'sets success flash' do
+        expect(flash[:successs]).to be_present
+      end
+      it 'redirects to offer index' do
+        expect(response).to redirect_to(admin_offers_path)
+      end
+    end
+    context 'with invalid params' do
+      before(:each) do
+        attributes[:name] = ''
+        post :create, attributes
+      end
+      it 'doesnt save offer' do
+        expect(assigns(:offer).new_record?).to eq true
+      end
+      it 'sets error flash' do
+        expect(flash[:danger]).to be_present
+      end
+      it 'renders new template' do
+        expect(response).to render_template('new')
+      end
+    end
+  end
+
+  describe 'GET edit' do
+    it 'assings proper offer' do
+      @offer = FactoryGirl.create(:offer)
+      get :edit
+      expect(assigns(:offer)).to eq(@offer)
+    end
+  end
+
+  describe 'PUT update' do
+    let(:attributes) {
+      attributes = FactoryGirl.attributes_for(:offer)
+      attributes['name'] = 'new_name'
+      attributes
+    }
+    before(:each) do
+      @offer = FactoryGirl.create(:offer)
+    end
+    context 'with valid params' do
+      before(:each) do
+        put :update, {id: @offer.to_param, offer: attributes}
+      end
+      it 'assigns user' do
+        expect(assigns(:offer)).to eq(@offer)
+      end
+      it 'updates offer' do
+        @offer.reload
+        expect(@offer.name).to eq('new_name')
+      end
+      it 'sets success flash' do
+        expect(flash[:success]).to be_present
+      end
+      it 'redirects to offer index' do
+        expect(response).to redirect_to admin_offers_path
+      end
+    end
+    context 'with invalid params' do
+      before(:each) do
+        attributes['name'] = ''
+        put :update, {id: @offer.to_param, offer: attributes}
+      end
+      it 'assigns offer' do
+        expect(assigns[:offer]).to eq(@offer)
+      end
+      it 'sets error flash' do
+        expect(flash[:danger]).to be_present
+      end
+      it 'rerendes edit template' do
+        expect(response).to render_template('edit')
+      end
+    end
+
+  end
+
 end
