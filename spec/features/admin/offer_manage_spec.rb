f1b4ff6e27e257878e8a005c64c5a3fa3c2c1989
Karol Lipski <karol.lipski@gmail.com>
2016-04-01 19:49:12 +0200

    create edit offer request spec

--- spec/features/admin/offer_manage_spec.rb
+++ spec/features/admin/offer_manage_spec.rb
@@ -12,3 +12,58 @@ feature 'offer listing' do
     end
   end
 end
+
+feature 'create offer' do
+  before(:each) do
+    allow_any_instance_of(AdminController).to receive(:authenticate)
+    visit new_admin_offer_path
+  end
+  context 'when data is valid' do
+    before(:each) do
+      fill_in 'offer_name', with: 'oferta testowa'
+      fill_in 'offer_start_date', with: '2012-01-03'
+      fill_in 'offer_end_date', with: '2016-01-01'
+      click_button 'Zapisz'
+    end
+    scenario 'display succes info' do
+      expect(page.html).to include("toastr.success('Oferta została dodana','',opts);")
+    end
+    scenario 'redirects to offer listing' do
+      expect(page).to have_selector 'h3', text: 'Oferty'
+    end
+  end
+  context 'when data is invalid' do
+    scenario 'display error info' do
+      click_button 'Zapisz'
+      expect(page.html).to include("toastr.error('Błąd zapisu','',opts);")
+    end
+  end
+end
+
+feature 'edit offer' do
+  before(:each) do
+    allow_any_instance_of(AdminController).to receive(:authenticate)
+    @offer = FactoryGirl.create(:offer)
+    visit edit_admin_offer_path @offer
+  end
+  context 'When data is valid' do
+    before(:each) do
+      fill_in 'offer_name', with: 'offerxxx'
+      click_button 'Zapisz'
+    end
+    scenario 'display success info' do
+      expect(page.html).to include("toastr.success('Oferta została zmieniona','',opts);")
+    end
+    scenario 'it redirects to offer list' do
+      expect(page).to have_selector 'h3', text: 'Oferty'
+    end
+  end
+  context 'when data is invalid' do
+    scenario 'display error info' do
+      fill_in 'offer_name', with: ''
+      click_button 'Zapisz'
+      expect(page.html).to include("toastr.error('Błąd zapisu','',opts);")
+    end
+  end
+
+end
