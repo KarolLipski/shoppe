3869829d88219ba03ae5e81f66f438ecbf4b63d0
Karol Lipski <karol.lipski@gmail.com>
2017-01-18 20:52:40 +0100

    css buttons

--- app/datatables/offer_item_datatable.rb
+++ app/datatables/offer_item_datatable.rb
@@ -6,9 +6,10 @@ class OfferItemDatatable < ItemDatatable
     Item.joins(:offer_items).joins("LEFT JOIN categories ON items.category_id = categories.id").where("stored_items.offer_id = ?", @offer_id).references(:offer_items)
   end
 
+  #@todo this button should change only offer item activity
   def changeActiveButton(item)
     text = (item.active) ? 'Aktywny' : 'Nieaktywny'
-    css = (item.active) ? "btn btn-green btn-xs" : "btn btn-red btn-xs"
+    css = (item.active) ? "btn btn-success btn-xs" : "btn btn-danger btn-xs"
     @view.link_to text, '#', :class => css, :id => "change-active-#{item.id}"
   end
 
