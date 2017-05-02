c8946d1e9d366a8bbc7fe30ad70cff2dd9142f43
Karol Lipski <karol.lipski@gmail.com>
2017-05-02 11:44:05 +0200

    main root page object

--- spec/features/main_page_spec.rb
+++ spec/features/main_page_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 feature 'Main page' do
-  scenario 'show all data'
+  scenario 'show all reccomendation' do
+    main_page = Pages::MainRoot.new
+    main_page.visit_page
+    expect(main_page).to be_on_page
+    expect(page).to have_selector('.best-seller-pro', count: 3)
+  end
 end
